3e30a35c21d15d7018e9d54bed39956b57b27dc4
Eric Mrak <mail@ericmrak.info>
2016-01-26 22:59:10 -0800

    initialize a GLFW window

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,32 @@
-module Lib
-    ( ptui
-    ) where
+module Lib (ptui) where
 
 import Args
 import Settings
+import Control.Monad (when)
+import Control.Concurrent
+import qualified Graphics.UI.GLFW as GLFW
 import qualified Data.Ini as I (readIniFile)
 
 ptui :: Args -> IO ()
 ptui a = do
-  configContents <- I.readIniFile (config a)
-  let settings = case configContents of
-                      Left s -> error s
-                      Right i -> fromINI i
-  print settings
+    configContents <- I.readIniFile (config a)
+    let settings = either error fromINI configContents
+
+    window <- makeWindow 800 600 "ptui"
+    threadDelay 10000000
+    GLFW.destroyWindow window
+    GLFW.terminate
+
+
+makeWindow :: Int -> Int -> String -> IO GLFW.Window
+makeWindow w h title = do
+    initialized <- GLFW.init
+    if initialized
+       then do
+            mw <- GLFW.createWindow w h title Nothing Nothing
+            case mw of
+                Nothing -> error "Could not create a window"
+                Just w -> do
+                    GLFW.makeContextCurrent mw
+                    pure w
+       else error "Could not initialize GLFW"
