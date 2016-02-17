9707fe35ff007764b10cf135e193784c51c7987f
Eric Mrak <mail@ericmrak.info>
2016-02-16 23:03:42 -0800

    Adding cursor movement parsing

--- src/lib/Pt/StateMachine.hs
+++ src/lib/Pt/StateMachine.hs
@@ -3,92 +3,159 @@
 {-# LANGUAGE PatternSynonyms #-}
 module Pt.StateMachine where
 
-import Data.ByteString.Lazy.Char8
+import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Char (isDigit)
+import Data.List (foldl',uncons)
+import Data.Maybe (catMaybes,fromMaybe)
+import Text.Read (readMaybe)
 
 data State = Q0
            | QESC
-           | QSGR
-           | QSGR2
-           | QSGR3
-           | QSGR4
-           | QSGREND
+           | QCSI
+           | QSGR0
+           | QSGR38
+           | QSGR38c
+           | QSGR38r
+           | QSGR38g
+           | QSGR38b
+           | QSGR48
+           | QSGR48c
+           | QSGR48r
+           | QSGR48g
+           | QSGR48b
 
-data SGR = Bold Bool
-         | Faint
-         | Blink Bool
-         | Underscore Bool
-         | Italic Bool
-         | Reverse Bool
-         | Foreground Int
-         | Background Int
-         | Invisible Bool
-         | Strikethrough Bool
-         | DoubleUnderline
-         | Reset
-         deriving Show
+data Color = Color256 Int
+           | Trucolor Int Int Int
+           | Default
+           deriving Show
+
+data SGRAttr = Reset
+             | Bold Bool
+             | Faint
+             | Blink Bool
+             | Underscore Bool
+             | Italic Bool
+             | Reverse Bool
+             | Foreground Color
+             | Background Color
+             | Invisible Bool
+             | Strikethrough Bool
+             | DoubleUnderline
+             deriving Show
 
 data Command = Noop
              | Output Char
-             | SGRs [SGR]
-             | End
+             | SGR [SGRAttr]
+             | CUU Int
+             | CUD Int
+             | CUF Int
+             | CUB Int
+             | CNL Int
+             | CPL Int
+             | CHA Int
+             | CHT Int
+             | CUP Int Int
              deriving Show
 
-pattern b :> bs <- (uncons -> Just (b,bs))
-pattern Empty   <- (uncons -> Nothing)
+pattern b :> bs <- (B.uncons -> Just (b,bs))
+pattern Empty   <- (B.uncons -> Nothing)
 
-transduce :: State -> Command -> ByteString -> [Command]
+transduce :: State -> (String,[String]) -> B.ByteString -> [Command]
 transduce _ _ Empty         = []
-transduce Q0 _ ('\x1b':>bs) = transduce QESC Noop bs
-transduce Q0 _ (b:>bs)      = Output 'x' : transduce Q0 Noop bs
+transduce Q0 t ('\x1b':>bs) = transduce QESC t bs
+transduce Q0 t (b:>bs)      = Output b : transduce Q0 t bs
 
-transduce QESC _ ('[':>bs) = transduce QSGR (SGRs []) bs
+transduce QESC _ ('[':>bs) = transduce QCSI ("",[]) bs
 
-transduce QSGR  (SGRs l) ('0':>bs) = transduce QSGREND (SGRs (l ++ [Reset])) bs
-transduce QSGR  (SGRs l) ('1':>bs) = transduce QSGREND (SGRs (l ++ [Bold True])) bs
-transduce QSGR  c        ('2':>bs) = transduce QSGR2 c bs
-transduce QSGR  c        ('3':>bs) = transduce QSGR3 c bs
-transduce QSGR  c        ('4':>bs) = transduce QSGR4 c bs
-transduce QSGR  (SGRs l) ('5':>bs) = transduce QSGREND (SGRs (l ++ [Blink True])) bs
-transduce QSGR  (SGRs l) ('7':>bs) = transduce QSGREND (SGRs (l ++ [Reverse True])) bs
-transduce QSGR  (SGRs l) ('8':>bs) = transduce QSGREND (SGRs (l ++ [Invisible True])) bs
-transduce QSGR  (SGRs l) ('9':>bs) = transduce QSGREND (SGRs (l ++ [Strikethrough True])) bs
+transduce QCSI (p,ps) ('A':>bs) = CUU (fromMaybe 1 $ readMaybe p) : transduce Q0 ("",[]) bs
+transduce QCSI (p,ps) ('B':>bs) = CUD (fromMaybe 1 $ readMaybe p) : transduce Q0 ("",[]) bs
+transduce QCSI (p,ps) ('C':>bs) = CUF (fromMaybe 1 $ readMaybe p) : transduce Q0 ("",[]) bs
+transduce QCSI (p,ps) ('D':>bs) = CUB (fromMaybe 1 $ readMaybe p) : transduce Q0 ("",[]) bs
+transduce QCSI (p,ps) ('E':>bs) = CNL (fromMaybe 1 $ readMaybe p) : transduce Q0 ("",[]) bs
+transduce QCSI (p,ps) ('F':>bs) = CPL (fromMaybe 1 $ readMaybe p) : transduce Q0 ("",[]) bs
+transduce QCSI (p,ps) ('G':>bs) = CHA (fromMaybe 1 $ readMaybe p) : transduce Q0 ("",[]) bs
+transduce QCSI (p,ps) ('H':>bs) = makeCUP (ps++[p]) : transduce Q0 ("",[]) bs
+transduce QCSI (p,ps) ('I':>bs) = CHT (fromMaybe 1 $ readMaybe p) : transduce Q0 ("",[]) bs
+transduce QCSI (p,ps) ('m':>bs) = makeSGR (ps++[p]) : transduce Q0 ("",[]) bs
+transduce QCSI (p,ps) (';':>bs) = transduce QCSI ("",ps++[p]) bs
+transduce QCSI (p,ps) (b:>bs) | isDigit b = transduce QCSI (p++[b],ps) bs
+                              | otherwise = transduce Q0 (p,ps) bs
+transduce _ t (b:>bs) = transduce Q0 t bs
 
-transduce QSGR2 (SGRs l) ('1':>bs) = transduce QSGREND (SGRs (l ++ [DoubleUnderline])) bs
-transduce QSGR2 (SGRs l) ('2':>bs) = transduce QSGREND (SGRs (l ++ [Bold False])) bs
-transduce QSGR2 (SGRs l) ('3':>bs) = transduce QSGREND (SGRs (l ++ [Italic False])) bs
-transduce QSGR2 (SGRs l) ('4':>bs) = transduce QSGREND (SGRs (l ++ [Underscore False])) bs
-transduce QSGR2 (SGRs l) ('5':>bs) = transduce QSGREND (SGRs (l ++ [Blink False])) bs
-transduce QSGR2 (SGRs l) ('7':>bs) = transduce QSGREND (SGRs (l ++ [Reverse False])) bs
-transduce QSGR2 (SGRs l) ('8':>bs) = transduce QSGREND (SGRs (l ++ [Invisible False])) bs
-transduce QSGR2 (SGRs l) ('9':>bs) = transduce QSGREND (SGRs (l ++ [Strikethrough False])) bs
-transduce QSGR2 (SGRs l) (';':>bs) = transduce QSGR (SGRs (l ++ [Faint])) bs
-transduce QSGR2 (SGRs l) ('m':>bs) = SGRs (l ++ [Faint]) : transduce Q0 Noop bs
+makeCUP :: [String] -> Command
+makeCUP [] = CUP 1 1
+makeCUP (r:[]) = CUP (fromMaybe 1 $ readMaybe r) 1
+makeCUP (r:c:_) = CUP (fromMaybe 1 $ readMaybe r) (fromMaybe 1 $ readMaybe c)
 
-transduce QSGR3 (SGRs l) ('0':>bs) = transduce QSGREND (SGRs (l ++ [Foreground 0])) bs
-transduce QSGR3 (SGRs l) ('1':>bs) = transduce QSGREND (SGRs (l ++ [Foreground 1])) bs
-transduce QSGR3 (SGRs l) ('2':>bs) = transduce QSGREND (SGRs (l ++ [Foreground 2])) bs
-transduce QSGR3 (SGRs l) ('3':>bs) = transduce QSGREND (SGRs (l ++ [Foreground 3])) bs
-transduce QSGR3 (SGRs l) ('4':>bs) = transduce QSGREND (SGRs (l ++ [Foreground 4])) bs
-transduce QSGR3 (SGRs l) ('5':>bs) = transduce QSGREND (SGRs (l ++ [Foreground 5])) bs
-transduce QSGR3 (SGRs l) ('6':>bs) = transduce QSGREND (SGRs (l ++ [Foreground 6])) bs
-transduce QSGR3 (SGRs l) ('7':>bs) = transduce QSGREND (SGRs (l ++ [Foreground 7])) bs
-transduce QSGR3 (SGRs l) ('9':>bs) = transduce QSGREND (SGRs (l ++ [Foreground 9])) bs
-transduce QSGR3 (SGRs l) (';':>bs) = transduce QSGR (SGRs (l ++ [Italic True])) bs
-transduce QSGR3 (SGRs l) ('m':>bs) = SGRs (l ++ [Italic True]) : transduce Q0 Noop bs
+makeSGR :: [String] -> Command
+makeSGR = SGR . transduceSGR QSGR0 [] . catMaybes . map (readMaybe :: String -> Maybe Int)
+    where transduceSGR _ _ [] = []
+          transduceSGR QSGR0 l (0:xs) = Reset : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (1:xs) = Bold True : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (2:xs) = Faint : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (3:xs) = Italic True : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (4:xs) = Underscore True : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (5:xs) = Blink True : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (7:xs) = Reverse True : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (8:xs) = Invisible True : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (9:xs) = Strikethrough True : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (21:xs) = DoubleUnderline : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (22:xs) = Bold False : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (23:xs) = Italic False : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (24:xs) = Underscore False : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (25:xs) = Blink False : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (27:xs) = Reverse False : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (28:xs) = Invisible False : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (29:xs) = Strikethrough False : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (30:xs) = Foreground (Color256 0) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (31:xs) = Foreground (Color256 1) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (32:xs) = Foreground (Color256 2) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (33:xs) = Foreground (Color256 3) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (34:xs) = Foreground (Color256 4) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (35:xs) = Foreground (Color256 5) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (36:xs) = Foreground (Color256 6) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (37:xs) = Foreground (Color256 7) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (38:xs) = transduceSGR QSGR38 l xs
+          transduceSGR QSGR0 l (39:xs) = Foreground Default : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (40:xs) = Background (Color256 0) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (41:xs) = Background (Color256 1) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (42:xs) = Background (Color256 2) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (43:xs) = Background (Color256 3) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (44:xs) = Background (Color256 4) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (45:xs) = Background (Color256 5) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (46:xs) = Background (Color256 6) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (47:xs) = Background (Color256 7) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (48:xs) = transduceSGR QSGR48 l xs
+          transduceSGR QSGR0 l (49:xs) = Background Default : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (90:xs) = Foreground (Color256 0) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (91:xs) = Foreground (Color256 1) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (92:xs) = Foreground (Color256 2) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (93:xs) = Foreground (Color256 3) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (94:xs) = Foreground (Color256 4) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (95:xs) = Foreground (Color256 5) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (96:xs) = Foreground (Color256 6) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (97:xs) = Foreground (Color256 7) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (100:xs) = Background (Color256 0) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (101:xs) = Background (Color256 1) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (102:xs) = Background (Color256 2) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (103:xs) = Background (Color256 3) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (104:xs) = Background (Color256 4) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (105:xs) = Background (Color256 5) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (106:xs) = Background (Color256 6) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (107:xs) = Background (Color256 7) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR0 l (_:xs) = transduceSGR QSGR0 l xs
 
-transduce QSGR4 (SGRs l) ('0':>bs) = transduce QSGREND (SGRs (l ++ [Background 0])) bs
-transduce QSGR4 (SGRs l) ('1':>bs) = transduce QSGREND (SGRs (l ++ [Background 1])) bs
-transduce QSGR4 (SGRs l) ('2':>bs) = transduce QSGREND (SGRs (l ++ [Background 2])) bs
-transduce QSGR4 (SGRs l) ('3':>bs) = transduce QSGREND (SGRs (l ++ [Background 3])) bs
-transduce QSGR4 (SGRs l) ('4':>bs) = transduce QSGREND (SGRs (l ++ [Background 4])) bs
-transduce QSGR4 (SGRs l) ('5':>bs) = transduce QSGREND (SGRs (l ++ [Background 5])) bs
-transduce QSGR4 (SGRs l) ('6':>bs) = transduce QSGREND (SGRs (l ++ [Background 6])) bs
-transduce QSGR4 (SGRs l) ('7':>bs) = transduce QSGREND (SGRs (l ++ [Background 7])) bs
-transduce QSGR4 (SGRs l) ('9':>bs) = transduce QSGREND (SGRs (l ++ [Background 9])) bs
-transduce QSGR4 (SGRs l) (';':>bs) = transduce QSGR (SGRs (l ++ [Underscore True])) bs
-transduce QSGR4 (SGRs l) ('m':>bs) = SGRs (l ++ [Underscore True]) : transduce Q0 Noop bs
+          transduceSGR QSGR38 l (5:xs) = transduceSGR QSGR38c l xs
+          transduceSGR QSGR38 l (2:xs) = transduceSGR QSGR38r l xs
+          transduceSGR QSGR38c l (x:xs) = Foreground (Color256 x) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR38r _ (r:xs) = transduceSGR QSGR38g [r] xs
+          transduceSGR QSGR38g (r:[]) (g:xs) = transduceSGR QSGR38b [r,g] xs
+          transduceSGR QSGR38b (r:g:[]) (b:xs) = Foreground (Trucolor r g b) : transduceSGR QSGR0 [] xs
 
-transduce QSGREND c (';':>bs) = transduce QSGR c bs
-transduce QSGREND c ('m':>bs) = c : transduce Q0 Noop bs
-
-transduce _ _ (b :> bs)       = transduce Q0 Noop bs
+          transduceSGR QSGR48 l (5:xs) = transduceSGR QSGR48c l xs
+          transduceSGR QSGR48 l (2:xs) = transduceSGR QSGR48r l xs
+          transduceSGR QSGR48c l (x:xs) = Background (Color256 x) : transduceSGR QSGR0 l xs
+          transduceSGR QSGR48r _ (r:xs) = transduceSGR QSGR48g [r] xs
+          transduceSGR QSGR48g (r:[]) (g:xs) = transduceSGR QSGR48b [r,g] xs
+          transduceSGR QSGR48b (r:g:[]) (b:xs) = Background (Trucolor r g b) : transduceSGR QSGR0 [] xs
+          transduceSGR _ _ (_:xs) = transduceSGR QSGR0 [] xs
